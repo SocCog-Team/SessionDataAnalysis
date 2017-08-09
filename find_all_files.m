bde3d7026b6516a67f1062bf35ba86a236954fcd
Sebastian Moeller <moeller0@gmx.de>
2017-08-09 20:38:24 +0200

    "Make the verbosity configurable via function argument. If this argument is omitted use the old verbose output."

--- find_all_files.m
+++ find_all_files.m
@@ -1,38 +1,52 @@
-function fileList = find_all_files(folder, filePattern)
-  % Get list of all subfolders.
-  allSubFolders = genpath(folder);
-  % Parse into a cell array.
-  remain = allSubFolders;
-  folderNameList = {};
-  while true
+function fileList = find_all_files(folder, filePattern, VerbosityLevel)
+
+
+if ~exist('VerbosityLevel', 'var')
+    VerbosityLevel = 1;
+end
+
+
+
+% Get list of all subfolders.
+allSubFolders = genpath(folder);
+% Parse into a cell array.
+remain = allSubFolders;
+folderNameList = {};
+while true
     [singleSubFolder, remain] = strtok(remain, pathsep); % or use pathsep
     if isempty(singleSubFolder)
-      break;
+        break;
     end
     folderNameList = [folderNameList singleSubFolder];
-  end
-  nFolder = length(folderNameList);
-  
-  fileList = {};
-  % Process all files in those folders.
-  for iFolder = 1:nFolder
+end
+nFolder = length(folderNameList);
+
+fileList = {};
+% Process all files in those folders.
+for iFolder = 1:nFolder
     % Get this folder and print it out.
     thisFolder = folderNameList{iFolder};
-    fprintf('Processing folder %s\n', thisFolder);
-	
+    if (VerbosityLevel)
+        fprintf('Processing folder %s\n', thisFolder);
+    end
+    
     % Get ALL files.
     currentPattern = [thisFolder, filesep, filePattern];
-    baseFileNames = dir(currentPattern);	
+    baseFileNames = dir(currentPattern);
     nFile = length(baseFileNames);
     if nFile >= 1
-      % Go through all those files.
-      for iFile = 1:nFile
-        fullFileName = fullfile(thisFolder, baseFileNames(iFile).name);
-        fprintf('     Processing file %s\n', fullFileName);
-        fileList = [fileList fullFileName];
-      end
+        % Go through all those files.
+        for iFile = 1:nFile
+            fullFileName = fullfile(thisFolder, baseFileNames(iFile).name);
+            if (VerbosityLevel)
+                fprintf('     Processing file %s\n', fullFileName);
+            end
+            fileList = [fileList fullFileName];
+        end
     else
-      fprintf('     Folder %s has no files in it.\n', thisFolder);
+        if (VerbosityLevel)           
+            fprintf('     Folder %s has no files in it.\n', thisFolder);
+        end
     end
-  end  
+end
 end
