e089be1fa8e3b60dbf8796593ddc56488d87314e
Sebastian Moeller <moeller0@gmx.de>
2017-11-28 17:23:26 +0100

    "Add a few sessions to exclude (these old sessions needs more checking for the input parsing... but are not really relevant for the current analyses, so skip them for now.)"

--- subject_bias_analysis_sm01.m
+++ subject_bias_analysis_sm01.m
@@ -100,7 +100,7 @@ end
 %TODO fix up the parser to deal with older well-formed report files, switch
 %to selective exclusion of individual days instead of whole months...
 ExcludeWildCardList = {'_TESTVERSIONS', '20170106', '201701', '201702', '201703', 'A_SM-InactiveVirusScanner', 'A_Test', 'TestA', 'TestB', 'B_Test'};
-ExcludeWildCardList = {'ANALYSES', '201701', '201702', '201703', '20170406', 'A_SM-InactiveVirusScanner', 'A_Test', 'TestA', 'TestB', 'B_Test', '_PARKING', '_TESTVERSIONS'};
+ExcludeWildCardList = {'ANALYSES', '201701', '201702', '201703', '20170403', '20170405', '20170405', '20170406', 'A_SM-InactiveVirusScanner', 'A_Test', 'TestA', 'TestB', 'B_Test', '_PARKING', '_TESTVERSIONS'};
 
 IncludedFilesIdx = [];
 for iFile = 1 : length(experimentFile)
