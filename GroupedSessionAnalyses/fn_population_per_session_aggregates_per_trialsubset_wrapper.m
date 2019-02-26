0c10308c845d9e525817c034554af3e76e45a419
Sebastian Moeller <moeller0@gmx.de>
2019-02-26 10:34:35 +0100

    "Add local_get_column_name_indices function."

--- GroupedSessionAnalyses/fn_population_per_session_aggregates_per_trialsubset_wrapper.m
+++ GroupedSessionAnalyses/fn_population_per_session_aggregates_per_trialsubset_wrapper.m
@@ -71,7 +71,7 @@ if isempty(coordination_metrics_table) || (recalc_coordination_metrics)
         coordination_metrics_table.info_struct = info;
         coordination_metrics_table.data = tmp_coordination_metrics_table.row;
         coordination_metrics_table.header = tmp_coordination_metrics_table.header;
-        coordination_metrics_table.cn = get_column_name_indices(tmp_coordination_metrics_table.header);
+        coordination_metrics_table.cn = local_get_column_name_indices(tmp_coordination_metrics_table.header);
         coordination_metrics_table.cfg_struct = tmp_coordination_metrics_table.cfg_struct;
         coordination_metrics_table.coordination_metrics_struct = tmp_coordination_metrics_table.coordination_metrics_struct;
     else
@@ -129,5 +129,30 @@ else
     coordination_metrics_table.coordination_metrics_struct(tmp_key_idx) = tmp_coordination_metrics_table.coordination_metrics_struct;
 end
 
+return
+end
+
+
+function [columnnames_struct, n_fields] = local_get_column_name_indices(name_list, start_val)
+% return a structure with each field for each member if the name_list cell
+% array, giving the position in the name_list, then the columnnames_struct
+% can serve as to address the columns, so the functions assigning values
+% to the columns do not have to care too much about the positions, and it
+% becomes easy to add fields.
+% name_list: cell array of string names for the fields to be added
+% start_val: numerical value to start the field values with (if empty start
+%            with 1 so the results are valid indices into name_list)
+
+if nargin < 2
+    start_val = 1;  % value of the first field
+end
+n_fields = length(name_list);
+for i_col = 1 : length(name_list)
+    cur_name = name_list{i_col};
+    % skip empty names, this allows non consequtive numberings
+    if ~isempty(cur_name)
+        columnnames_struct.(cur_name) = i_col + (start_val - 1);
+    end
+end
 return
 end
