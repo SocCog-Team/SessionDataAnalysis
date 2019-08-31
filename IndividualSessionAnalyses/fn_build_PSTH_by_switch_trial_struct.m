abdc1d1f32cb61596c6e249a5f9866e46613dc33
Sebastian Moeller <moeller0@gmx.de>
2019-09-01 00:50:13 +0200

    "Fixes."

--- IndividualSessionAnalyses/fn_build_PSTH_by_switch_trial_struct.m
+++ IndividualSessionAnalyses/fn_build_PSTH_by_switch_trial_struct.m
@@ -43,12 +43,15 @@ for i_switch_type = 1 : length(selected_choice_combinaton_pattern_list)
 	n_switches = length(switch_trial_struct.(current_switch_type_string));
 	pertrial_raw_array = nan([n_switches, (n_pre_bins + 1 + n_post_bins)]);
 	pertrial_padded_array = pertrial_raw_array;
+	% so we always return at least empty filelds
+	pertrial_perswitch_struct.(current_switch_type_string) = [];
+	aggregate_perswitch_struct.(current_switch_type_string) = [];
+
 	if (n_switches)
 		% to extend the pattern we need to know what to look for
 		current_pattern_start_char = current_switch_type_string(1);
-		current_pattern_stop_char = current_switch_type_string(end);
-		
-		cur_switchtype_idx_list = switch_trial_struct.(current_switch_type_string);
+		current_pattern_stop_char = current_switch_type_string(end);		
+		cur_switchtype_idx_list = switch_trial_struct.(current_switch_type_string);		
 		for i_switch = 1 : n_switches
 			cur_switch_idx = cur_switchtype_idx_list(i_switch);
 			% extract the classifier data
@@ -59,23 +62,27 @@ for i_switch_type = 1 : length(selected_choice_combinaton_pattern_list)
 			
 			cur_switch_start_bin_idx_offset = 0;
 			cur_switch_end_bin_idx_offset = 0;
-			if (cur_switch_start_bin_idx < 1)
-				n_missing_bins = 1 - cur_switch_start_bin_idx;
-				current_PeriEventTimeData_padded = [nan([n_missing_bins, 1]); data(1:cur_switch_end_bin_idx)];
-				current_PeriEventTimeData_raw = nan(size(current_PeriEventTimeData_padded)); % mark event as incomplete	
-				cur_switch_start_bin_idx_offset = n_missing_bins;
-			elseif (cur_switch_end_bin_idx > length(data))
-				n_missing_bins = cur_switch_end_bin_idx - length(data);
-				current_PeriEventTimeData_padded = [data(cur_switch_start_bin_idx:end); nan([n_missing_bins, 1])];
-				if (length(current_PeriEventTimeData_padded) ~= 8)
-					disp('Doh...');
-				end
-				current_PeriEventTimeData_raw = nan(size(current_PeriEventTimeData_padded)); % mark event as incomplete	
-				cur_switch_end_bin_idx_offset = n_missing_bins;
-			else
-				current_PeriEventTimeData_raw = data(cur_switch_start_bin_idx:cur_switch_end_bin_idx);
+			
+			% effectively too short a data set
+			if (cur_switch_start_bin_idx < 1) && (cur_switch_end_bin_idx > length(data))
+				current_PeriEventTimeData_raw = nan([(n_pre_bins + 1 + n_post_bins), 1]);
 				current_PeriEventTimeData_padded = current_PeriEventTimeData_raw;
-			end			
+			else
+				if (cur_switch_start_bin_idx < 1)
+					n_missing_bins = 1 - cur_switch_start_bin_idx;
+					current_PeriEventTimeData_padded = [nan([n_missing_bins, 1]); data(1:cur_switch_end_bin_idx)];
+					current_PeriEventTimeData_raw = nan(size(current_PeriEventTimeData_padded)); % mark event as incomplete
+					cur_switch_start_bin_idx_offset = n_missing_bins;
+				elseif (cur_switch_end_bin_idx > length(data))
+					n_missing_bins = cur_switch_end_bin_idx - length(data);
+					current_PeriEventTimeData_padded = [data(cur_switch_start_bin_idx:end); nan([n_missing_bins, 1])];
+					current_PeriEventTimeData_raw = nan(size(current_PeriEventTimeData_padded)); % mark event as incomplete
+					cur_switch_end_bin_idx_offset = n_missing_bins;
+				else
+					current_PeriEventTimeData_raw = data(cur_switch_start_bin_idx:cur_switch_end_bin_idx);
+					current_PeriEventTimeData_padded = current_PeriEventTimeData_raw;
+				end
+			end
 			
 			if (strict_pattern_extension)
 				% look into the past
@@ -110,24 +117,4 @@ for i_switch_type = 1 : length(selected_choice_combinaton_pattern_list)
 	end
 end
 
-end
-
-function [ aggregate_peri_event_data_struct ] = fn_aggregate_event_data( data_table )
-% 
-nan_handling = 'omitnan';%5 includenan or omitnan
-processing_dim = 1;
-std_weight = 0;
-alpha = 0.05; % for confidence interval halfwidth
-
-% now just aggregate over the data
-aggregate_peri_event_data_struct.median = median(data_table, processing_dim, nan_handling);
-aggregate_peri_event_data_struct.max = max(data_table, [], processing_dim, nan_handling);
-aggregate_peri_event_data_struct.min = min(data_table, [], processing_dim, nan_handling);
-aggregate_peri_event_data_struct.mean = mean(data_table, processing_dim, nan_handling);
-aggregate_peri_event_data_struct.std = std(data_table, std_weight, processing_dim, nan_handling);
-aggregate_peri_event_data_struct.n = sum(~isnan(data_table), processing_dim);
-aggregate_peri_event_data_struct.cihw = calc_cihw(aggregate_peri_event_data_struct.std, aggregate_peri_event_data_struct.n, alpha);
-
-
-return
 end
