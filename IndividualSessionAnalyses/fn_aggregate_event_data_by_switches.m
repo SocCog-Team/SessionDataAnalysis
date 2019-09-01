e3416284d5ba8b3064ef6250d534cd0b43f4bfd4
Sebastian Moeller <moeller0@gmx.de>
2019-09-01 02:58:45 +0200

    "Fixes."

---
+++ IndividualSessionAnalyses/fn_aggregate_event_data_by_switches.m
@@ -0,0 +1,14 @@
+function [ aggregate_struct ] = fn_aggregate_event_data_by_switches( per_trial_struct )
+%FN_AGGREGATE_EVENT_DATA_BY_SWITCHES Summary of this function goes here
+%   Detailed explanation goes here
+
+current_fieldnames = fieldnames(per_trial_struct);
+
+for i_switch = 1 : length(current_fieldnames)
+	current_switch_type = current_fieldnames{i_switch};
+	aggregate_struct.(current_switch_type).raw = fn_aggregate_event_data(per_trial_struct.(current_switch_type).raw);
+	aggregate_struct.(current_switch_type).nan_padded = fn_aggregate_event_data(per_trial_struct.(current_switch_type).nan_padded);
+end
+
+end
+
