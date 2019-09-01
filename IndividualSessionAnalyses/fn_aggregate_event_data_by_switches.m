fe1d51d9268b55b12ec1f94f4f3cfe467a41d659
Sebastian Moeller <moeller0@gmx.de>
2019-09-01 15:08:21 +0200

    "Fixes."

--- IndividualSessionAnalyses/fn_aggregate_event_data_by_switches.m
+++ IndividualSessionAnalyses/fn_aggregate_event_data_by_switches.m
@@ -6,8 +6,12 @@ current_fieldnames = fieldnames(per_trial_struct);
 
 for i_switch = 1 : length(current_fieldnames)
 	current_switch_type = current_fieldnames{i_switch};
-	aggregate_struct.(current_switch_type).raw = fn_aggregate_event_data(per_trial_struct.(current_switch_type).raw);
-	aggregate_struct.(current_switch_type).nan_padded = fn_aggregate_event_data(per_trial_struct.(current_switch_type).nan_padded);
+	if isempty(per_trial_struct.(current_switch_type))
+		aggregate_struct.(current_switch_type) = [];
+	else
+		aggregate_struct.(current_switch_type).raw = fn_aggregate_event_data(per_trial_struct.(current_switch_type).raw);
+		aggregate_struct.(current_switch_type).nan_padded = fn_aggregate_event_data(per_trial_struct.(current_switch_type).nan_padded);
+	end
 end
 
 end
