e978d43578d022e56a6d4b847f3d4d8d70f35cc6
Sebastian Moeller <moeller0@gmx.de>
2020-11-15 14:01:32 +0100

    "Changed handling of pre and post bins that exceed the actual trials, by pre-expanding the classifoier string and the data array, instead of trying to fix up actual problematic cases individually. Sinc the function does not return trial numbers that is safe."

--- IndividualSessionAnalyses/fn_build_PSTH_by_switch_trial_struct.m
+++ IndividualSessionAnalyses/fn_build_PSTH_by_switch_trial_struct.m
@@ -1,6 +1,9 @@
 function [ aggregate_perswitch_struct, pertrial_perswitch_struct ] = fn_build_PSTH_by_switch_trial_struct( trial_idx, choice_combination_color_string, selected_choice_combinaton_pattern_list, data, pattern_alignment_offset, n_pre_bins, n_post_bins, strict_pattern_extension, pad_mismatch_with_nan)
 %FN_BUILD_PSTH_BY_SWITCH_TRIAL_STRUCT Summary of this function goes here
 %   Detailed explanation goes here
+% instead of trying to deal with pre and post bins as explicit corner cases
+% we simply extend both the classifier string and the data array/list by
+% the approriate pre and postbins (char(0), and NaN respectively)
 
 % to collect the aggregate, nanmean, nanstd, CI per bin and switch
 aggregate_perswitch_struct = [];
@@ -33,7 +36,22 @@ if ~exist('pad_mismatch_with_nan', 'var') || isempty(pad_mismatch_with_nan)
 	pad_mismatch_with_nan = 1;
 end
 
+
+% TODO: extend choice_combination_color_string (and data) by n_pre_bins spaces (NaNs) at the beginning and
+% n_post_bins spaces (NaNs) at the end to allow to find pre and post bins
+choice_pre_bin_array = char(zeros([1, n_pre_bins]));
+data_pre_bin_array = NaN * (ones([n_pre_bins, 1]));
+choice_post_bin_array = char(zeros([1, n_post_bins]));
+data_post_bin_array = NaN * (ones([n_post_bins, 1]));
+orig_choice_combination_color_string = choice_combination_color_string;
+choice_combination_color_string = [choice_pre_bin_array, orig_choice_combination_color_string, choice_post_bin_array];
+data = [data_pre_bin_array; data; data_post_bin_array];
+
+
 % first collect the actual switch trial indices
+% ATTENTION these trial indices are only valid inside the current choice_combination_color_string
+% and do not reflect "real" trial numbers valid externally, but since we do
+% not return trial numbers at all, this is currently save.
 switch_trial_struct = fn_extract_switches_from_classifier_string(choice_combination_color_string, selected_choice_combinaton_pattern_list);
 
 
@@ -65,15 +83,18 @@ for i_switch_type = 1 : length(selected_choice_combinaton_pattern_list)
 			
 			% effectively too short a data set
 			if (cur_switch_start_bin_idx < 1) && (cur_switch_end_bin_idx > length(data))
+				error('This should not trigger anymore, since we pre extend slassifier string and data array.');
 				current_PeriEventTimeData_raw = nan([(n_pre_bins + 1 + n_post_bins), 1]);
 				current_PeriEventTimeData_padded = current_PeriEventTimeData_raw;
 			else
 				if (cur_switch_start_bin_idx < 1)
+					error('This should not trigger anymore, since we pre extend slassifier string and data array.');
 					n_missing_bins = 1 - cur_switch_start_bin_idx;
 					current_PeriEventTimeData_padded = [nan([n_missing_bins, 1]); data(1:cur_switch_end_bin_idx)];
 					current_PeriEventTimeData_raw = nan(size(current_PeriEventTimeData_padded)); % mark event as incomplete
 					cur_switch_start_bin_idx_offset = n_missing_bins;
 				elseif (cur_switch_end_bin_idx > length(data))
+					error('This should not trigger anymore, since we pre extend slassifier string and data array.');
 					n_missing_bins = cur_switch_end_bin_idx - length(data);
 					current_PeriEventTimeData_padded = [data(cur_switch_start_bin_idx:end); nan([n_missing_bins, 1])];
 					current_PeriEventTimeData_raw = nan(size(current_PeriEventTimeData_padded)); % mark event as incomplete
@@ -86,7 +107,8 @@ for i_switch_type = 1 : length(selected_choice_combinaton_pattern_list)
 			
 			if (strict_pattern_extension) && ~((cur_switch_start_bin_idx < 1) && (cur_switch_end_bin_idx > length(data)))
 				% look into the past
-				tmp_start_idx = cur_switch_idx - (n_pre_bins - cur_switch_start_bin_idx_offset) + 1;	
+				tmp_start_idx = cur_switch_idx - (n_pre_bins - cur_switch_start_bin_idx_offset) + 1;
+				tmp_start_idx = max([tmp_start_idx, 1]); % stay inside the arry!
 				tmp_end_idx = cur_switch_idx;
 				mismatch_char_idx = find(choice_combination_color_string(tmp_start_idx:tmp_end_idx) ~= current_pattern_start_char);
 				if ~isempty(mismatch_char_idx)
@@ -114,8 +136,17 @@ for i_switch_type = 1 : length(selected_choice_combinaton_pattern_list)
 		nan_padded_pattern = tmp_string;
 		nan_padded_pattern(1:end) = current_pattern_start_char;
 		nan_padded_pattern(n_pre_bins + pattern_alignment_offset:end) = current_pattern_stop_char;
-		raw_pattern(n_pre_bins: n_pre_bins - pattern_alignment_offset + length(current_switch_type_string)) = current_switch_type_string;
-		nan_padded_pattern(n_pre_bins: n_pre_bins - pattern_alignment_offset + length(current_switch_type_string)) = current_switch_type_string;
+		
+		
+		%raw_pattern(n_pre_bins: n_pre_bins - pattern_alignment_offset + length(current_switch_type_string)) = current_switch_type_string;
+		%nan_padded_pattern(n_pre_bins: n_pre_bins - pattern_alignment_offset + length(current_switch_type_string)) = current_switch_type_string;
+
+		raw_pattern(n_pre_bins - pattern_alignment_offset + 1: n_pre_bins - pattern_alignment_offset + length(current_switch_type_string)) = current_switch_type_string;
+		nan_padded_pattern(n_pre_bins - pattern_alignment_offset + 1: n_pre_bins - pattern_alignment_offset + length(current_switch_type_string)) = current_switch_type_string;
+
+		
+		%raw_pattern(n_pre_bins + 1: n_pre_bins + 1 + length(current_switch_type_string)) = current_switch_type_string;
+		%nan_padded_pattern(n_pre_bins + 1: n_pre_bins + 1 + length(current_switch_type_string)) = current_switch_type_string;
 				
 		pertrial_perswitch_struct.(current_switch_type_string).raw = pertrial_raw_array;
 		pertrial_perswitch_struct.(current_switch_type_string).nan_padded = pertrial_padded_array;
