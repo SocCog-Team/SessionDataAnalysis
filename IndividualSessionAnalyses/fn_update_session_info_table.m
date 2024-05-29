0a1ffde50a98b86704ccc6748fa3c04d3a4fa6df
taskcontroller <taskcontroller@PC40687.dpz.lokal>
2024-05-29 11:35:37 +0200

    Deal with excel opening tables in exclusive mode making overwriting impossible.
    This does not really fix the isue but works around it...

--- IndividualSessionAnalyses/fn_update_session_info_table.m
+++ IndividualSessionAnalyses/fn_update_session_info_table.m
@@ -1,4 +1,4 @@
-function [ ] = fn_update_session_info_table(existing_session_info_table_FQN, new_data_struct_array, sort_by_field_name)
+function [ output_data_table ] = fn_update_session_info_table(existing_session_info_table_FQN, new_data_struct_array, sort_by_field_name)
 %FN_UPDATE_SESSION_INFO_TABLE Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -24,24 +24,35 @@ sort_key_list = {data_struct_array(:).(sort_by_field_name)};
 [unique_key_list, unique_record_idx] = unique(sort_key_list, 'last');
 output_data_struct_array = data_struct_array(unique_record_idx);
 
-% save as mat file:
+% save struct array as mat file:
 disp(['Saving session info data as: ' existing_session_info_table_FQN]);
-
 save(existing_session_info_table_FQN, 'output_data_struct_array');
 
 % now save as xlsx and csv
 output_data_table = struct2table(output_data_struct_array);
-
 [out_dir, out_name, out_ext] = fileparts(existing_session_info_table_FQN);
 
-
 % save as excel file
-writetable(output_data_table, fullfile(out_dir, [out_name, '.xlsx']));
-% save as TXT
+disp(['Saving session info data as .xslx: ' fullfile(out_dir, [out_name, '.xlsx'])]);
+try
+	writetable(output_data_table, fullfile(out_dir, [out_name, '.xlsx']));
+catch
+	% likely open in blocking mode, save as emergency file with date time
+	cur_datetime_string = char(datetime("now", 'Format', 'yyyyMMdd-HHmmss'));
+	disp([mfilename, ': Excel table version of session info data, is not writable; saving instead as: ',  [out_name, '.', cur_datetime_string, '.xlsx']])
+	writetable(output_data_table, fullfile(out_dir, [out_name, '.', cur_datetime_string, '.xlsx']));
+end
+	
+	% save as TXT
+disp(['Saving session info data as .txt: ' fullfile(out_dir, [out_name, '.txt'])]);
 writetable(output_data_table, fullfile(out_dir, [out_name, '.txt']), 'Delimiter', ';');
 % save as CSV
+disp(['Saving session info data as .csv: ' fullfile(out_dir, [out_name, '.csv'])]);
 writetable(output_data_table, fullfile(out_dir, [out_name, '.csv']), 'Delimiter', ',');
 
+% save the matlab table as .table.mat file
+disp(['Saving session info data as matlab table: ' fullfile(out_dir, [out_name, '.table.mat'])]);
+save(fullfile(out_dir, [out_name, '.table.mat']), 'output_data_table');
 return
 end
 
